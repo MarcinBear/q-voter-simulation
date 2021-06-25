65482fea77237c28f2633b4ff39553ca25aa8fdd
Marcin Miśkiewicz <>
2021-06-26 00:09:17 +0200

    stiff mcs, xlabel, code cleaning

--- app.py
+++ app.py
@@ -10,30 +10,36 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, update_title=None)
 server = app.server
 
+# ---------------- starting parameters ----------------
 n = 25
 speed = 100
 start_data = np.random.choice([-1, 1], size=(n, n))
 
 fig = go.Figure()
-fig.add_trace(go.Heatmap(z=np.random.choice([-1, 1], (n, n)),
-                         colorscale=[[0, "rgb(235, 103, 103)"],
-                                     [0.9, "rgb(103, 230, 114)"],
-                                     [1, "rgb(103, 230, 114)"]]))
+fig.add_trace(
+    go.Heatmap(z=np.random.choice([-1, 1], (n, n)),
+               colorscale=[[0, "rgb(235, 103, 103)"],
+                           [0.9, "rgb(103, 230, 114)"],
+                           [1, "rgb(103, 230, 114)"]])
+            )
 
 fig.update_layout(
                   showlegend=False, autosize=True,
-                  title={
-                            'text': "q-voter model simulation",
-                            'y': 0.95,
-                            'x': 0.5,
-                            'font': {'size': 40},
-                            'xanchor': 'center',
-                            'yanchor': 'top'},
+                  title={'text': "q-voter model simulation",
+                         'y': 0.95,
+                         'x': 0.5,
+                         'font': {'size': 40},
+                         'xanchor': 'center',
+                         'yanchor': 'top'},
                   )
 
 fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=[0], y=[np.sum(start_data)/(n*n)], line=dict(color="rgb(103, 230, 114)", width=2)))
-fig2.update_layout(showlegend=True, autosize=True, title="Average opinion graph")
+fig2.add_trace(
+    go.Scatter(x=[0], y=[np.sum(start_data)/(n*n)],
+               line=dict(color="rgb(103, 230, 114)", width=2))
+              )
+
+fig2.update_layout(showlegend=True, autosize=True, title="Average opinion graph", xaxis_title="MCS")
 
 app.layout = html.Div(id="page", children=[
         dcc.Store(id='value_T'),
@@ -63,46 +69,46 @@ app.layout = html.Div(id="page", children=[
                             options=[
                                 {'label': 'conformity + independence', 'value': 'c_i'},
                                 {'label': 'conformity + anticonformity', 'value': 'c_a'},
-                            ],
+                                    ],
                             value='c_i',
-                           ),
-                           dcc.RadioItems(
+                          ),
+                          dcc.RadioItems(
                             id='draw',
                             options=[
                                 {'label': 'drawing with repetitions⠀⠀', 'value': 'with'},
                                 {'label': 'drawing without repetitions', 'value': 'without'},
-                            ],
+                                    ],
                             value='with',
-                           ),
-                            dcc.RadioItems(
+                          ),
+                          dcc.RadioItems(
                             id='start_state',
                             options=[
                                 {'label': 'random', 'value': 'random'},
                                 {'label': 'circle', 'value': 'circle'},
                                 {'label': 'checkboard', 'value': 'checkboard'},
                                 {'label': 'solid', 'value': 'solid'}
-                            ],
+                                    ],
                             value='random',
                             labelStyle={'display': 'inline-block', "margin-left": "5%"}
-                           ),
-                      html.Label("q = ", id='q_label', style={'font-size': '20px'}),
-                      dcc.Input(
+                          ),
+                          html.Label("q = ", id='q_label', style={'font-size': '20px'}),
+                          dcc.Input(
                               id="q", type="number", placeholder=3, value=3,
                               min=1, max=8, step=1,
                           ),
-                      html.Label("p = ", id='p_label', style={'font-size': '20px'}),
-                      dcc.Input(
+                          html.Label("p = ", id='p_label', style={'font-size': '20px'}),
+                          dcc.Input(
                               id="p", type="number", placeholder=0.5, value=0.5,
                               min=0.01, max=1, step=0.01,
                           ),
-                      html.Label("f = ", id='f_label', style={'font-size': '20px'}),
-                      dcc.Input(
+                          html.Label("f = ", id='f_label', style={'font-size': '20px'}),
+                          dcc.Input(
                               id="f", type="number", placeholder=0.5, value=0.5,
                               min=0.01, max=1, step=0.01,
                           ),
-                      html.Label(" ", id='bottom_label', style={'font-size': '20px'}),
-                      ])
-                     ]
+                          html.Label(" ", id='bottom_label', style={'font-size': '20px'}),
+                        ])
+                      ]
                     )
             ]
 )
@@ -142,13 +148,13 @@ def update_data(n_intervals,  figure, set_click, data, n, q, p, f, start_state):
         new_data = state, p, f, q, n, start_state
         y_new = np.sum((np.array(new_data[0])))/(n*n)
         t = go.Scatter(x=[1], y=[y_new], line=dict(color="rgb(103, 230, 114)", width=2))
-        l = go.Layout(showlegend=False, autosize=True, title="Average opinion graph")
+        l = go.Layout(showlegend=False, autosize=True, title="Average opinion graph", xaxis_title={'text': "MCS"})
 
         return (dict(z=[new_data[0]]), [0], new_data[4]), {'data': [t], 'layout': l}, new_data
 
     # heatmap
     M, p, f, q, n, start_state = data
-    for agent in range(n):
+    for agent in range(50):
         i, j = np.random.randint(0, n, size=2)
         nbs = [M[i % n][(j + 1) % n],
                M[i % n][(j - 1) % n],
@@ -170,7 +176,7 @@ def update_data(n_intervals,  figure, set_click, data, n, q, p, f, start_state):
     y_new = figure['data'][0]['y']
     y_new.append(np.sum((np.array(data[0])))/(n*n))
     t = go.Scatter(x=list(range(n_intervals + 1)), y=y_new, line=dict(color="rgb(103, 230, 114)", width=2))
-    l = go.Layout(showlegend=False, autosize=True, title="Average opinion graph")
+    l = go.Layout(showlegend=False, autosize=True, title="Average opinion graph", xaxis_title={'text': "MCS"})
 
     return (dict(z=[M]), [0], n), {'data': [t], 'layout': l}, (M, p, f, q, n, start_state)
 
